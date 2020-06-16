4a053e33d98eb92d3bf62185f1d9910c89c5d013
Armen Markossyan <hello@armen.im>
2020-06-16 19:49:49 +0200

    Pass the port number

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker_run="docker run -d -p $INPUT_PORT:4567 vsouza/kinesis-local:$INPUT_VERSION"
+docker_run="docker run -d -p $INPUT_PORT:4567 vsouza/kinesis-local:$INPUT_VERSION --port 4567"
 
 sh -c "$docker_run"

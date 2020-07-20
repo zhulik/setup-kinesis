7ca8f4e833fa129b13dd2a7dc60e529f11013a77
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2020-07-20 13:07:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker_run="docker run -d -p $INPUT_PORT:4567 vsouza/kinesis-local:$INPUT_VERSION --port 4567"
+docker_run="docker run -d -p $INPUT_PORT:4567 vsouza/kinesis-local:$INPUT_VERSION --port 4567 --createStreamMs 0 --deleteStreamMs 0"
 
 sh -c "$docker_run"

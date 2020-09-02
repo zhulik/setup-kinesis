dcef07049d4a6f78a51ef695a1ccaf3f46fb3176
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2020-09-02 16:18:34 +0200

    Migrate to tddmonkey/kinesis-local

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker_run="docker run -d -p $INPUT_PORT:4567 vsouza/kinesis-local:$INPUT_VERSION --port 4567 --createStreamMs 0 --deleteStreamMs 0"
+docker_run="docker run -d -p $INPUT_PORT:4567 tddmonkey/kinesis-local:$INPUT_VERSION --port 4567 --createStreamMs 0 --deleteStreamMs 0"
 
 sh -c "$docker_run"
